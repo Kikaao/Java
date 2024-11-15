8c3bc931f91ea34cd77d2ad02081855bd0c74eb1
Stamatis Kaxiris <150450551+Kikaao@users.noreply.github.com>
2024-11-15 10:29:47 +0200

    Add files via upload

---
+++ Lab4_P1/src/Main.java
@@ -0,0 +1,124 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+
+//---------------------1------------------------
+
+//        int sum=0;
+//        for (int i=0; i<101; i++){
+//            sum+=i;
+//            System.out.println(sum);
+//        }
+//        System.out.println("The sum is " + sum);
+
+
+//---------------------2---------------------
+
+//        int sum=0;
+//        int i = 0;
+//
+//        while (true){
+//            i++;
+//            sum+=i;
+//            if (i==100)
+//                break;
+//        }
+//        System.out.println(sum);
+
+//-------------------3--------------------------
+
+//        int sum=0;
+//        int i = 0;
+//
+//        do {
+//            i++;
+//            sum+=i;
+//            if (i==100)
+//                break;
+//        }while(true);
+//
+//        System.out.println(sum);
+
+//-------------------4--------------------------
+
+//        int sum = 0;
+//
+//        while (true){
+//            System.out.print("Type a positive number: ");
+//            int n = read.nextInt();
+//            if(n<0)
+//                System.out.println("Wrong number.");
+//            else if (n>0){
+//                sum+=n;
+//                System.out.println(sum);
+//            } else if (n==0)
+//                break;
+//        }
+//        System.out.println("End!!!!!\n" + sum);
+
+//---------------------------5---------------------------
+
+//        int sum = 0;
+//        int i =0;
+//
+//        while (true){
+//            System.out.print("Type a positive number: ");
+//            int n = read.nextInt();
+//            if(n<0)
+//                System.out.println("Wrong number.");
+//            else if (n>0){
+//                sum+=n;
+//                System.out.println(sum);
+//                i++;
+//            } else if (n==0)
+//                break;
+//        }
+//        System.out.println("End!!!!!\n" + sum);
+//        System.out.println(sum/i);
+
+//----------------------6-------------------
+
+//        while (true) {
+//            System.out.print("Enter a year: ");
+//            int year = read.nextInt();
+//
+//            if (year % 4 == 0) {
+//                if (year % 400 == 0) {
+//                    System.out.println("It's a leap year.");
+//                } else if (year % 100 == 0)
+//                    System.out.println("It's not a leap year.");
+//                else
+//                    System.out.println("It's a leap year.");
+//            } else if (year==-1) {
+//                break;
+//            } else
+//                System.out.println("It's not a leap year.");
+//        }
+
+//------------------------7--------------------------
+
+//        System.out.print("Type a number: ");
+//        int x = read.nextInt();
+//
+//        for (int i = 1;i<11;i++){
+//            System.out.println(x*i);
+//        }
+
+//---------------------8-----------------------------
+
+//        System.out.print("Type a number: ");
+//        int x = read.nextInt();
+//        int val = x;
+//
+//        for (int i = 1; i<x;i++){
+//            val = i*val;
+//            System.out.println(val);
+//        }
+//
+
+
+
+    }
+}
